c5e02072679c8ee9fc57b8f9dc26fcc0669494b3
xavir <xaviranik.me@gmail.com>
2020-06-05 11:52:43 +0600

    Initial commit

---
+++ coding-ninja.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* Plugin Name:       Coding Ninja
+* Plugin URI:        https://zabiranik.me
+* Description:       Basic plugin for Wordpress
+* Version:           1.0.0
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            Zabir Anik
+* Author URI:        https://zabiranik.me
+* License:           GPL v2 or later
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain:       coding-ninja
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
