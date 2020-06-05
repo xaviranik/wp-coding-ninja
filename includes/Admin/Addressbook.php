d6d771aed8337b72b2e1cc153aadedfe8c692f89
xavir <xaviranik.me@gmail.com>
2020-06-05 16:44:24 +0600

    Addess: Addressbook Submenu page

---
+++ includes/Admin/Addressbook.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WeDevs\Ninja\Admin;
+
+/**
+ * Addressbook Class
+ */
+class Addressbook
+{
+	
+	public function plugin_page() {
+		echo "HELLO FROM ADRESSBOOK";
+	}
+}
--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -2,6 +2,8 @@
 
 namespace WeDevs\Ninja\Admin;
 
+use WeDevs\Ninja\Admin\Addressbook;
+
 /**
  * Menu handler class
  */
@@ -12,10 +14,19 @@ class Menu {
 	}
 
 	public function admin_menu() {
-		add_menu_page( __( 'Coding Ninja', 'coding-ninja' ), __( 'Coding Ninja', 'coding-ninja' ), 'manage_options', 'coding_ninja', [ $this, 'plugin_page' ], 'dashicons-universal-access-alt' );
+		$parent_slug = 'coding_ninja';
+		$capability = 'manage_options';
+		add_menu_page( __( 'Coding Ninja', 'coding-ninja' ), __( 'Coding Ninja', 'coding-ninja' ), $capability, $parent_slug, [ $this, 'addressbook_page' ], 'dashicons-universal-access-alt' );
+		add_submenu_page( $parent_slug, __( 'Address Book', 'coding-ninja' ), __( 'Address Book', 'coding-ninja' ), $capability, $parent_slug, [ $this, 'addressbook_page' ] );
+		add_submenu_page( $parent_slug, __( 'Settings', 'coding-ninja' ), __( 'Settings', 'coding-ninja' ), $capability, 'coding_ninja_settings', [ $this, 'settings_page' ] );
 	}
 
-	public function plugin_page() {
-		echo "PLUGIN PAGE";
+	public function settings_page() {
+		echo "SETTINGS PAGE";
+	}
+
+	public function addressbook_page() {
+		$addressbook = new Addressbook();
+		$addressbook->plugin_page();
 	}
 }
